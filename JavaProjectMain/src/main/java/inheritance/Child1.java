2a5c879fb16f23f31883cb02c52a97c2ae4f0ba5
DELL <DELL@DESKTOP-623ERR5>
2025-02-06 23:12:00 +0530

    inheritance

---
+++ JavaProjectMain/src/main/java/inheritance/Child1.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class Child1 extends parent1
+{
+String name="Anu";	
+
+	
+	public void display()
+	{
+		System.out.println(name);
+	
+	System.out.println(super.name);
+	}
+	public static void main(String args[])
+	{
+
+	Child1 obj1=new Child1();
+	obj1.display();
+
+}}// TO REFER THE PARENTS CLASS INSTANCE VARIABLE
