75c1c2e3d82ab797f9be38b77785ac44475dc632
DELL <DELL@DESKTOP-623ERR5>
2025-02-06 23:31:50 +0530

    programs

--- JavaProjectMain/src/main/java/string/StringMethods.java
+++ JavaProjectMain/src/main/java/string/StringMethods.java
@@ -5,16 +5,16 @@ public class StringMethods {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 String a="Hello";
-String b="Hhis is java program";//contains
+String b="This is java program";//contains
 String c="Hello";
 System.out.println(a);
 System.out.println(a.charAt(1));//print single char
-System.out.println(a.length());//print length of the char
-System.out.println(a.concat(" world"));
-System.out.println(b.contains("This"));
-System.out.println(c.equals(a));
-System.out.println(c.equalsIgnoreCase(a));
-System.out.println(c.compareTo(b));
+System.out.println("length-"+a.length());//print length of the char
+System.out.println("concat- "+a.concat(" world"));
+System.out.println("contains- "+b.contains("This"));
+System.out.println("equals- "+c.equals(a));
+System.out.println("equals ignore- "+c.equalsIgnoreCase(a));
+System.out.println("compare to- "+c.compareTo(b));
 	}
 
 }
